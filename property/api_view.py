23a9fddb2a337b42fca94bf809b65edd0140dde0
Julia <riyamtechcampus@gmail.com>
2022-03-17 07:57:54 -0700

    add permissions to property and blog apps

--- property/api_view.py
+++ property/api_view.py
@@ -1,15 +1,17 @@
 from .models import Property
 from .serializers import PropertySerializer
 from rest_framework.generics import ListAPIView , ListCreateAPIView ,  RetrieveAPIView , RetrieveUpdateDestroyAPIView
-
+from rest_framework.permissions import IsAuthenticated
 
 
 class PropertyAPIList(ListCreateAPIView):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
+    permission_classes = IsAuthenticated
 
 
 
 class PropertyAPIDetail(RetrieveUpdateDestroyAPIView):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
+    permission_classes = IsAuthenticated
